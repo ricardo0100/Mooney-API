b473a849303e418b7b48e6e3d7adcd1ef69a3964
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2017-02-04 12:16:05 -0200

    Finally a BaseModel for resources

--- Sources/App/Controllers/TransactionsController.swift
+++ Sources/App/Controllers/TransactionsController.swift
@@ -1,97 +1,97 @@
-import Foundation
-import Vapor
-import HTTP
-
-final class TransactionsController: ResourceRepresentable {
-    
-    typealias Model = Transaction
-    
-    public func makeResource() -> Resource<Transaction> {
-        return Resource(
-            index: index,
-            store: store,
-            show: show,
-            replace: update,
-            destroy: destroy
-        )
-    }
-    
-    func index(request: Request) throws -> ResponseRepresentable {
-        let user = try getUserFromRequest(request: request)
-        let category = try Transaction.query().filter("user_id", user.id!).all()
-        let json = try JSON(node: category)
-        return json
-    }
-    
-    func store(request: Request) throws -> ResponseRepresentable {
-        let user = try getUserFromRequest(request: request)
-        
-        guard let name = request.data["name"]?.string else {
-            throw Abort.custom(status: Status.badRequest, message: "Name not provided")
-        }
-        
-        guard let categoryId = request.data["category_id"]?.int else {
-            throw Abort.custom(status: Status.badRequest, message: "Category ID not provided")
-        }
-        
-        guard let category = try Category.query().filter("id", categoryId).first() else {
-            throw Abort.custom(status: Status.badRequest, message: "Category not found")
-        }
-        
-        if (category.userId != user.id) {
-            throw Abort.custom(status: Status.badRequest, message: "Category not found")
-        }
-        
-        guard let accountId = request.data["account_id"]?.int else {
-            throw Abort.custom(status: Status.badRequest, message: "Account ID not provided")
-        }
-        
-        guard let account = try Account.query().filter("id", accountId).first() else {
-            throw Abort.custom(status: Status.badRequest, message: "Account not found")
-        }
-        
-        if (account.userId != user.id) {
-            throw Abort.custom(status: Status.badRequest, message: "Account not found")
-        }
-        
-        var transaction = try Transaction(name: name, userId: user.id, categoryId: category.id, accountId: account.id)
-        try transaction.save()
-        
-        let id: Int = transaction.id!.int!
-        return "Transaction created with ID: \(id)"
-    }
-    
-    func show(request: Request, item transaction: Transaction) throws -> ResponseRepresentable {
-        let user = try getUserFromRequest(request: request)
-        if (user.id != transaction.userId) {
-            throw Abort.custom(status: Status.badRequest, message: "Authentication error")
-        }
-        return try JSON(node: transaction)
-    }
-    
-    func update(request: Request, item transaction: Transaction) throws -> ResponseRepresentable {
-        let user = try getUserFromRequest(request: request)
-        if (user.id != transaction.userId) {
-            throw Abort.custom(status: Status.badRequest, message: "Authentication error")
-        }
-        
-        var updatedCategory = transaction
-        
-        guard let name = request.data["name"]?.string else {
-            throw Abort.custom(status: Status.badRequest, message: "Name not provided")
-        }
-        
-        updatedCategory.name = try name.validated(by: NotEmpty())
-        updatedCategory.updatedAt = try Date().timestamp().validated(by: Timestamp())
-        
-        try updatedCategory.save()
-        
-        let id: Int = updatedCategory.id!.int!
-        return "Category with ID: \(id) updated"
-    }
-    
-    func destroy(request: Request, item user: Transaction) throws -> ResponseRepresentable {
-        return user
-    }
-    
-}
+//import Foundation
+//import Vapor
+//import HTTP
+//
+//final class TransactionsController: ResourceRepresentable {
+//    
+//    typealias Model = Transaction
+//    
+//    public func makeResource() -> Resource<Transaction> {
+//        return Resource(
+//            index: index,
+//            store: store,
+//            show: show,
+//            replace: update,
+//            destroy: destroy
+//        )
+//    }
+//    
+//    func index(request: Request) throws -> ResponseRepresentable {
+//        let user = try getUserFromRequest(request: request)
+//        let category = try Transaction.query().filter("user_id", user.id!).all()
+//        let json = try JSON(node: category)
+//        return json
+//    }
+//    
+//    func store(request: Request) throws -> ResponseRepresentable {
+//        let user = try getUserFromRequest(request: request)
+//        
+//        guard let name = request.data["name"]?.string else {
+//            throw Abort.custom(status: Status.badRequest, message: "Name not provided")
+//        }
+//        
+//        guard let categoryId = request.data["category_id"]?.int else {
+//            throw Abort.custom(status: Status.badRequest, message: "Category ID not provided")
+//        }
+//        
+//        guard let category = try Category.query().filter("id", categoryId).first() else {
+//            throw Abort.custom(status: Status.badRequest, message: "Category not found")
+//        }
+//        
+//        if (category.userId != user.id) {
+//            throw Abort.custom(status: Status.badRequest, message: "Category not found")
+//        }
+//        
+//        guard let accountId = request.data["account_id"]?.int else {
+//            throw Abort.custom(status: Status.badRequest, message: "Account ID not provided")
+//        }
+//        
+//        guard let account = try Account.query().filter("id", accountId).first() else {
+//            throw Abort.custom(status: Status.badRequest, message: "Account not found")
+//        }
+//        
+//        if (account.userId != user.id) {
+//            throw Abort.custom(status: Status.badRequest, message: "Account not found")
+//        }
+//        
+//        var transaction = try Transaction(name: name, userId: user.id, categoryId: category.id, accountId: account.id)
+//        try transaction.save()
+//        
+//        let id: Int = transaction.id!.int!
+//        return "Transaction created with ID: \(id)"
+//    }
+//    
+//    func show(request: Request, item transaction: Transaction) throws -> ResponseRepresentable {
+//        let user = try getUserFromRequest(request: request)
+//        if (user.id != transaction.userId) {
+//            throw Abort.custom(status: Status.badRequest, message: "Authentication error")
+//        }
+//        return try JSON(node: transaction)
+//    }
+//    
+//    func update(request: Request, item transaction: Transaction) throws -> ResponseRepresentable {
+//        let user = try getUserFromRequest(request: request)
+//        if (user.id != transaction.userId) {
+//            throw Abort.custom(status: Status.badRequest, message: "Authentication error")
+//        }
+//        
+//        var updatedCategory = transaction
+//        
+//        guard let name = request.data["name"]?.string else {
+//            throw Abort.custom(status: Status.badRequest, message: "Name not provided")
+//        }
+//        
+//        updatedCategory.name = try name.validated(by: NotEmpty())
+//        updatedCategory.updatedAt = try Date().timestamp().validated(by: Timestamp())
+//        
+//        try updatedCategory.save()
+//        
+//        let id: Int = updatedCategory.id!.int!
+//        return "Category with ID: \(id) updated"
+//    }
+//    
+//    func destroy(request: Request, item user: Transaction) throws -> ResponseRepresentable {
+//        return user
+//    }
+//    
+//}
--- Sources/App/Models/Account.swift
+++ Sources/App/Models/Account.swift
@@ -2,58 +2,34 @@ import Foundation
 import Vapor
 import Fluent
 
-final class Account: Model {
+class AccountModel: ModelBaseProtocol {
     
-    var id: Node?
-    var exists: Bool = false
+    static func tableName() -> String {
+        return "accounts"
+    }
+    
+    static func addFieldsToSchema(_ schema: Schema.Creator) throws {
+        schema.string("name")
+    }
+    
+}
+
+final class Account: ModelBase<AccountModel> {
+    
     var name: Valid<NotEmpty>
-    var createdAt: Valid<Timestamp>
-    var updatedAt: Valid<Timestamp>
-    
-    //Relationships
-    var userId: Node?
     
     init(name: String, userId: Node?) throws {
         self.name = try name.validated(by: NotEmpty())
-        self.createdAt = try Date().timestamp().validated(by: Timestamp())
-        self.updatedAt = try Date().timestamp().validated(by: Timestamp())
-        self.userId = userId
+        try super.init(userId: userId)
     }
     
-    init(node: Node, in context: Context) throws {
-        id = try node.extract("id")
+    required init(node: Node, in context: Context) throws {
         name = try (node.extract("name") as String).validated(by: NotEmpty())
-        createdAt = try (node.extract("createdAt") as String).validated(by: Timestamp())
-        updatedAt = try (node.extract("updatedAt") as String).validated(by: Timestamp())
-        userId = try node.extract("user_id")
+        try super.init(node: node, in: context)
     }
     
-    func makeNode(context: Context) throws -> Node {
-        return try Node(node: [
-            "id": id,
-            "name": name.value,
-            "createdAt": createdAt.value,
-            "updatedAt": updatedAt.value,
-            "user_id": userId
-        ])
-    }
-    
-    static func prepare(_ database: Database) throws {
-        try database.create("accounts") { account in
-            account.id()
-            account.string("name")
-            account.string("createdAt")
-            account.string("updatedAt")
-            account.parent(User.self, optional: false)
-        }
-    }
-    
-    static func revert(_ database: Database) throws {
-        try database.delete("accounts")
-    }
-    
-    func user() throws -> Parent<User> {
-        return try parent(userId)
+    override func customFields() -> [String : NodeRepresentable?] {
+        return ["name": name.value]
     }
     
 }
