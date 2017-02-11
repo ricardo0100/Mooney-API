fd0b28566f64c64bcce09485df43f27ff87c49df
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2017-02-11 10:35:39 -0200

    Heroku build test

--- Sources/App/Models/Validation/Timestamp.swift
+++ Sources/App/Models/Validation/Timestamp.swift
@@ -1,18 +1,17 @@
-import Foundation
 import Vapor
 
 class Timestamp: ValidationSuite {
     
     //Regex for: YYYY-MM-DD HH:MM:SS
-    static let regex = try! NSRegularExpression(pattern: "\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d", options: .caseInsensitive)
+//    static let regex = try! RegularExpression(pattern: "\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d", options: .caseInsensitive)
     
     public static func validate(input value: String) throws {
-        let x = self.regex.rangeOfFirstMatch(in: value, range: NSRange(location: 0, length: value.count))
-        //TODO: Validate if is valid date
-        let valid = x.location != NSNotFound
-        if !valid {
-            throw Abort.custom(status: .badRequest, message: "Invalid Timestamp")
-        }
+//        let x = self.regex.rangeOfFirstMatch(in: value, range: NSRange(location: 0, length: value.count))
+//        //TODO: Validate if is valid date
+//        let valid = x.location != NSNotFound
+//        if !valid {
+//            throw Abort.custom(status: .badRequest, message: "Invalid Timestamp")
+//        }
     }
     
 }
